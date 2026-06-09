1c42422c1b24722df9747ca0c586235fe03d8b1e
Makson <127390597+maxviruk@users.noreply.github.com>
2026-06-09 12:16:59 +0200

    Delete TheUserOfTheDayBot-v2 directory

--- TheUserOfTheDayBot-v2/src/main/java/com/UserOfTheDayBot/enums/DBColumns.java
+++
@@ -1,7 +0,0 @@
-package com.UserOfTheDayBot.enums;
-
-public enum DBColumns {
-    // день года, когда последний раз запускалась каждая из игр
-    user_of_the_day_run_day,
-    loser_of_the_day_run_day
-}
--- TheUserOfTheDayBot-v2/src/main/java/com/UserOfTheDayBot/enums/Games.java
+++
@@ -1,6 +0,0 @@
-package com.UserOfTheDayBot.enums;
-
-public enum Games {
-    user_of_the_day,
-    loser_of_the_day
-}
